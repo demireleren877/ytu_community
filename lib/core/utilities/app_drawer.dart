4c81b3cbbf8d3367239a4f4734e1c4a06bea9fff
Eren Demirel <l5819033@std.yildiz.edu.tr>
2022-01-25 01:05:08 +0300

    refactored

--- lib/core/utilities/app_drawer.dart
+++ lib/core/utilities/app_drawer.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:math_eng_community/core/app_constants/path_constants.dart';
 import 'package:kartal/kartal.dart';
+import 'package:math_eng_community/core/services/firebase_services.dart';
+import 'package:math_eng_community/core/utilities/centered_progress.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -9,20 +11,35 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
-          SizedBox(height: 100.h),
-          CircleAvatar(
-            radius: 40.r,
-            backgroundColor: Colors.transparent,
-            child: Image.asset(PathConstants.profileImagePathDark),
-          ),
-          SizedBox(height: 5.h),
-          Text(
-            "Eren Demirel",
-            style: context.textTheme.headline5,
-          ),
-        ],
+      child: StreamBuilder(
+        stream: FirebaseServices.user
+            .doc(FirebaseServices.auth.currentUser!.email)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          var output = snapshot.data;
+          if (snapshot.data == null) {
+            return const CenteredProgressIndicator();
+          } else if (!snapshot.hasData) {
+            return const CenteredProgressIndicator();
+          }
+          return Column(
+            children: [
+              SizedBox(height: 75.h),
+              CircleAvatar(
+                radius: 50.r,
+                backgroundColor: Colors.transparent,
+                backgroundImage: output["profileImageUrl"] == null
+                    ? const AssetImage(PathConstants.profileImagePathDark)
+                    : NetworkImage(output["profileImageUrl"]) as ImageProvider,
+              ),
+              SizedBox(height: 15.h),
+              Text(
+                output["name"],
+                style: context.textTheme.headline5,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:math_eng_community/core/app_constants/path_constants.dart';
+import 'package:math_eng_community/core/services/firebase_services.dart';
 import 'package:math_eng_community/core/utilities/app_drawer.dart';
+import 'package:math_eng_community/core/utilities/centered_progress.dart';
 import 'package:math_eng_community/feature/forum_page/forum_screen.dart';
 import 'package:math_eng_community/feature/home/viewmodel/home_viewmodel.dart';
 import 'package:math_eng_community/feature/lecture_content/lecture_content_screen.dart';
@@ -21,9 +24,26 @@ class HomeScreen extends StatelessWidget {
           child: Builder(
             builder: (context) => InkWell(
               onTap: () => Scaffold.of(context).openDrawer(),
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                child: Image.asset(PathConstants.profileImagePathDark),
+              child: StreamBuilder(
+                stream: FirebaseServices.user
+                    .doc(FirebaseServices.auth.currentUser!.email)
+                    .snapshots(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  var output = snapshot.data;
+                  if (snapshot.data == null) {
+                    return const CenteredProgressIndicator();
+                  } else if (!snapshot.hasData) {
+                    return const CenteredProgressIndicator();
+                  }
+                  return CircleAvatar(
+                    radius: 50.r,
+                    backgroundColor: Colors.transparent,
+                    backgroundImage: output["profileImageUrl"] == null
+                        ? const AssetImage(PathConstants.profileImagePathDark)
+                        : NetworkImage(output["profileImageUrl"])
+                            as ImageProvider,
+                  );
+                },
               ),
             ),
           ),
